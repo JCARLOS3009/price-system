6fa9b0ccd28592487d157c79c603505411b3e211
JOSE CARLOS BARRETO FILHO <85513751+JCARLOS3009@users.noreply.github.com>
2024-05-29 02:12:59 -0300

    Create Produto.java

---
+++ Produto.java
@@ -0,0 +1,16 @@
+public class Produto {
+    private double precoOriginal;
+    private Strategy strategy;
+
+    public Produto(double precoOriginal) {
+        this.precoOriginal = precoOriginal;
+    }
+
+    public void setStrategy(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public double calcularPreco() {
+        return strategy.calcularPreco(precoOriginal);
+    }
+}
